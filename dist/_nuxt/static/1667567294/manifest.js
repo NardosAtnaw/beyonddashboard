fe50351b29ceed8ee97179c044e0c69b36246a1e
nardos atnaw <nardidumessa@gmail.com>
2022-11-04 16:35:00 +0300

    basechanged

--- dist/_nuxt/static/1667567294/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fsignup","\u002Fviewschedule","\u002Faddschedule","\u002Fpreregistration","\u002Fdashboard","\u002Fregister","\u002Feditschedule"]})
---
+++ dist/_nuxt/static/1667568842/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fpreregistration","\u002Fdashboard","\u002Feditschedule","\u002Fregister","\u002Fviewschedule","\u002Fsignup","\u002Faddschedule"]})
