eb1d52b2be105bf37ef8605e0814af388e87cb16
nardos atnaw <nardidumessa@gmail.com>
2022-11-04 16:08:33 +0300

    color changed2

--- dist/_nuxt/static/1666337709/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Feditschedule","\u002Fregister","\u002Faddschedule","\u002Fdashboard","\u002Fviewschedule","\u002Fsignup","\u002Fpreregistration"]})
