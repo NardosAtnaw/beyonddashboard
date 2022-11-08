9932d40b87ee3cd43847ea50aaba3c55efdfc48b
nardos atnaw <nardidumessa@gmail.com>
2022-11-08 15:57:12 +0300

    export excel4

--- dist/_nuxt/19c1fed.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{792:function(n,w){}}]);
--- dist/_nuxt/static/1667911257/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fpreregistration","\u002Faddschedule","\u002Fregister","\u002Feditschedule","\u002Fviewschedule","\u002Fsignup","\u002Fdashboard"]})
