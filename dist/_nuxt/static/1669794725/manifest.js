dbec58637f896d61ed24c9258ee1cd1bf515c6c3
nardos atnaw <nardidumessa@gmail.com>
2022-11-30 11:00:38 +0300

    height updated2

--- dist/_nuxt/static/1669794725/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fpreregistration","\u002Feditschedule","\u002Fsignup","\u002Fregister","\u002Fviewschedule","\u002Fdashboard","\u002Faddschedule"]})
